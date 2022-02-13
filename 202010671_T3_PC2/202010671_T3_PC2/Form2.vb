9ef6c5c692eb520f9c206bdc08b2ec7b8f9bd7f3
202010671 <98621454+202010671@users.noreply.github.com>
2022-02-12 21:36:29 -0600

    Add files via upload
    TAREA 3

---
+++ 202010671_T3_PC2/202010671_T3_PC2/Form2.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form2
+
+    Private Sub Form2_FormClosed(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles MyBase.FormClosed
+        Form1.Show()
+    End Sub
+
+
+    Private Sub Form2_Load2(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        If Form1.COMPRADOLAR.Checked Then
+            Label1.Text = "La cantidad de tu compra es: " + Str(calcularCompra(Form1.TBCOMPRA.Text, TCDOLAR))
+        End If
+
+        If Form1.COMPRAPESO.Checked Then
+            Label1.Text = "La cantidad de tu compra es: " + Str(calcularCompra(Form1.TBCOMPRA.Text, TCPESO))
+        End If
+
+        If Form1.COMPRAEURO.Checked Then
+            Label1.Text = "La cantidad de tu compra es: " + Str(calcularCompra(Form1.TBCOMPRA.Text, TCEURO))
+        End If
+
+        If Form1.COMPRACOLONOS.Checked Then
+            Label1.Text = "La cantidad de tu compra es: " + Str(calcularCompra(Form1.TBCOMPRA.Text, TCCOLONOS))
+        End If
+
+    End Sub
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        If Form1.VENTADOLAR.Checked Then
+            Label2.Text = "La cantidad de tu venta es: " + Str(calcularVenta(Form1.TBVENTA.Text, TCDOLAR))
+        End If
+
+        If Form1.VENTAPESOS.Checked Then
+            Label2.Text = "La cantidad de tu venta es: " + Str(calcularVenta(Form1.TBVENTA.Text, TCPESO))
+        End If
+
+        If Form1.VENTAEURO.Checked Then
+            Label2.Text = "La cantidad de tu venta es: " + Str(calcularVenta(Form1.TBVENTA.Text, TCEURO))
+        End If
+
+        If Form1.VENTACOLONOS.Checked Then
+            Label2.Text = "La cantidad de tu venta es: " + Str(calcularVenta(Form1.TBVENTA.Text, TCCOLONOS))
+        End If
+
+
+    End Sub
+
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ 202010671_T3_PC2/202010671_T3_PC2/Module1.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+    Public Const TCDOLAR = 7.69
+    Public Const TCPESO = 0.38
+    Public Const TCEURO = 7.89
+    Public Const TCCOLONOS = 0.012
+
+    Const COMISIONCOMPRA = 0.025
+    Const COMISIONVENTA = 0.03
+
+    Dim total_parcial = 0
+    Dim total
+
+    Sub limpiar()
+        Form1.TBCOMPRA.Clear()
+        Form1.TBVENTA.Clear()
+        Form1.CBCOMPRA.Checked = False
+        Form1.CBVENTA.Checked = False
+
+        Form2.Label1.Text = "Limpio"
+    End Sub
+
+    Function calcularCompra(ByVal cantidadCompra As Double, ByVal TipoCompra As Double) As Double
+        If cantidadCompra > 0 Then
+            total_parcial = cantidadCompra * TipoCompra
+            total = total_parcial * COMISIONCOMPRA + total_parcial
+
+        End If
+
+        Return Math.Round(total, 2)
+    End Function
+
+
+
+    Function calcularVenta(ByVal cantidadVenta As Double, ByVal TipoVenta As Double) As Double
+        If cantidadVenta > 0 Then
+            total_parcial = cantidadVenta * TipoVenta
+            total = total_parcial * COMISIONVENTA + total_parcial
+
+        End If
+
+        Return Math.Round(total, 2)
+    End Function
+
+End Module
