fac812efc6267465ea35cf0b57badb71f57e537b
202010671 <98621454+202010671@users.noreply.github.com>
2022-03-19 23:37:12 -0600

    202010671_Parcial2

---
+++ 202010671_Parcial2/202010671_Supermercado.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TBCantidadDeseada = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TBCantidadInventario = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TBNombrePack = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CBProducto = New System.Windows.Forms.ComboBox()
+        Me.DGV1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AceptarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LimpiarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DataGridViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VectoresToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DGV1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TBCantidadDeseada)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.TBCantidadInventario)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TBNombrePack)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.CBProducto)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 24)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(579, 121)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'TBCantidadDeseada
+        '
+        Me.TBCantidadDeseada.Location = New System.Drawing.Point(505, 69)
+        Me.TBCantidadDeseada.Name = "TBCantidadDeseada"
+        Me.TBCantidadDeseada.Size = New System.Drawing.Size(68, 21)
+        Me.TBCantidadDeseada.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(306, 72)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(174, 16)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Cantidad Deseada en el Pack"
+        '
+        'TBCantidadInventario
+        '
+        Me.TBCantidadInventario.Location = New System.Drawing.Point(161, 69)
+        Me.TBCantidadInventario.Name = "TBCantidadInventario"
+        Me.TBCantidadInventario.Size = New System.Drawing.Size(68, 21)
+        Me.TBCantidadInventario.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(138, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Cantidad en Inventario"
+        '
+        'TBNombrePack
+        '
+        Me.TBNombrePack.Location = New System.Drawing.Point(113, 26)
+        Me.TBNombrePack.Name = "TBNombrePack"
+        Me.TBNombrePack.Size = New System.Drawing.Size(116, 21)
+        Me.TBNombrePack.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Nombre del Pack"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(306, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Producto"
+        '
+        'CBProducto
+        '
+        Me.CBProducto.FormattingEnabled = True
+        Me.CBProducto.Items.AddRange(New Object() {"Jugos", "Frituras", "Galletas", "Baterías"})
+        Me.CBProducto.Location = New System.Drawing.Point(386, 23)
+        Me.CBProducto.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.CBProducto.Name = "CBProducto"
+        Me.CBProducto.Size = New System.Drawing.Size(187, 24)
+        Me.CBProducto.TabIndex = 0
+        Me.CBProducto.Text = "Selección del Producto..."
+        '
+        'DGV1
+        '
+        Me.DGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column7, Me.Column4, Me.Column5, Me.Column6})
+        Me.DGV1.Location = New System.Drawing.Point(6, 189)
+        Me.DGV1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.DGV1.Name = "DGV1"
+        Me.DGV1.Size = New System.Drawing.Size(579, 166)
+        Me.DGV1.TabIndex = 4
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "No."
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "No. de Packs"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Sobrante"
+        Me.Column3.Name = "Column3"
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = "Descuento"
+        Me.Column7.Name = "Column7"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Ingresos esperados por el pack"
+        Me.Column4.Name = "Column4"
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Ingresos esperados por el producto sobrante"
+        Me.Column5.Name = "Column5"
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "Total"
+        Me.Column6.Name = "Column6"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AceptarToolStripMenuItem, Me.LimpiarToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(624, 24)
+        Me.MenuStrip1.TabIndex = 5
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AceptarToolStripMenuItem
+        '
+        Me.AceptarToolStripMenuItem.Name = "AceptarToolStripMenuItem"
+        Me.AceptarToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.AceptarToolStripMenuItem.Text = "Aceptar"
+        '
+        'LimpiarToolStripMenuItem
+        '
+        Me.LimpiarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DataGridViewToolStripMenuItem, Me.VectoresToolStripMenuItem})
+        Me.LimpiarToolStripMenuItem.Name = "LimpiarToolStripMenuItem"
+        Me.LimpiarToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
+        Me.LimpiarToolStripMenuItem.Text = "Limpiar"
+        '
+        'DataGridViewToolStripMenuItem
+        '
+        Me.DataGridViewToolStripMenuItem.Name = "DataGridViewToolStripMenuItem"
+        Me.DataGridViewToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.DataGridViewToolStripMenuItem.Text = "DataGridView"
+        '
+        'VectoresToolStripMenuItem
+        '
+        Me.VectoresToolStripMenuItem.Name = "VectoresToolStripMenuItem"
+        Me.VectoresToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.VectoresToolStripMenuItem.Text = "Vectores"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(41, 20)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(624, 411)
+        Me.Controls.Add(Me.DGV1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Font = New System.Drawing.Font("Century Gothic", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DGV1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents CBProducto As System.Windows.Forms.ComboBox
+    Friend WithEvents DGV1 As System.Windows.Forms.DataGridView
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents AceptarToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LimpiarToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DataGridViewToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents VectoresToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TBCantidadInventario As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TBNombrePack As System.Windows.Forms.TextBox
+    Friend WithEvents TBCantidadDeseada As System.Windows.Forms.TextBox
+    Friend WithEvents Column1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As System.Windows.Forms.DataGridViewTextBoxColumn
+
+End Class
---
+++ 202010671_Parcial2/202010671_Supermercado.vb
@@ -0,0 +1,96 @@
+﻿Public Class Form1
+    Private Sub AceptarToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AceptarToolStripMenuItem.Click
+        If CBProducto.SelectedIndex = -1 Then
+            MsgBox("Seleccione los campos faltantes")
+        End If
+
+        If Contador < 6 Then
+            Número(Contador) = Contador + 1
+            Producto(Contador) = CBProducto.SelectedItem
+        End If
+
+
+        If TBCantidadInventario.Text < TBCantidadDeseada.Text Then
+            MsgBox("La cantidad del inventario debe ser mayor a la cantidad deseada")
+        End If
+
+
+        Dim numero1 As Integer = TBCantidadInventario.Text
+        Dim numero2 As Integer = TBCantidadDeseada.Text
+
+        Dim división As Integer = (numero1 / numero2)
+
+        NúmeroPacks(Contador) = división
+        Sobrante(Contador) = numero1 Mod numero2
+
+
+
+
+        Select Case IngresosPacks(Contador)
+            Case "Jugos"
+                IngresosPacks(Contador) = NúmeroPacks(Contador) * PJugo
+            Case "Frituras"
+                IngresosPacks(Contador) = NúmeroPacks(Contador) * PFrituras
+            Case "Galletas"
+                IngresosPacks(Contador) = NúmeroPacks(Contador) * PGalletas
+            Case "Baterías"
+                IngresosPacks(Contador) = NúmeroPacks(Contador) * PBaterías
+        End Select
+
+
+        If TBCantidadInventario.Text >= 10 Then
+            Descuento(Contador) = IngresosPacks(Contador) * 0.1
+        ElseIf TBCantidadInventario.Text >= 5 Then
+            Descuento(Contador) = IngresosPacks(Contador) * 0.05
+        Else
+            Descuento(Contador) = IngresosPacks(Contador) * 0.03
+        End If
+
+
+        IngresosEsperadosporelPack(Contador) = IngresosPacks(Contador) - Descuento(Contador)
+
+
+
+        Select Case IngresosEsperadosporelProductoSobrante(Contador)
+            Case "Jugos"
+                IngresosEsperadosporelProductoSobrante(Contador) = Sobrante(Contador) * PJugo
+            Case "Frituras"
+                IngresosEsperadosporelProductoSobrante(Contador) = Sobrante(Contador) * PFrituras
+            Case "Galletas"
+                IngresosEsperadosporelProductoSobrante(Contador) = Sobrante(Contador) * PGalletas
+            Case "Baterías"
+                IngresosEsperadosporelProductoSobrante(Contador) = Sobrante(Contador) * PBaterías
+        End Select
+
+
+        Total(Contador) = IngresosEsperadosporelPack(Contador) + IngresosEsperadosporelProductoSobrante(Contador)
+
+
+        If DGV1.Rows.Add(Contador + 1, (Número(Contador)), (NúmeroPacks(Contador)), (System.Math.Round(Sobrante(Contador), 2)), (System.Math.Round(IngresosEsperadosporelPack(Contador), 2)), (System.Math.Round(IngresosEsperadosporelProductoSobrante(Contador), 2)), (System.Math.Round(Total(Contador), 2))) Then
+            Contador = Contador + 1
+        Else
+            MsgBox("Se ha llegado al límite de registros")
+        End If
+
+
+    End Sub
+
+    
+    Private Sub SalirToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SalirToolStripMenuItem.Click
+        If MsgBox("¿Desea Salir?", vbYesNo) = vbYes Then
+            End
+        End If
+
+    End Sub
+
+    Private Sub DataGridViewToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DataGridViewToolStripMenuItem.Click
+        DGV1.Rows.Clear()
+    End Sub
+
+    Private Sub VectoresToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles VectoresToolStripMenuItem.Click
+        CBProducto.Items.Clear()
+        TBCantidadDeseada.Clear()
+        TBCantidadInventario.Clear()
+        TBNombrePack.Clear()
+    End Sub
+End Class
